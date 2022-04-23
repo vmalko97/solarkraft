e790a4526462cfa117364d2629d79ad3eb7817c0
Vladyslav Malko <vmalko97@gmail.com>
2022-04-23 21:44:10 +0300

    homepage

---
+++ wp-content/themes/yootheme-child/home.php
@@ -0,0 +1,369 @@
+<?php
+/*
+Template Name: Homepage
+*/
+
+get_header();
+?>
+<div>
+
+    <img src="/wp-content/uploads/2022/04/hero.jpg" alt="">
+
+    <h3>Välkommen till en <br> smartare solarlösning</h3>
+
+    <p>Driv ditt hem med solpaneler - smart & enkelt</p>
+
+
+    <p><a href="#">Få Offert</a></p>
+
+</div>
+<h2>Installationer</h2>
+<ul>
+    <li>
+
+        <img src="/wp-content/uploads/yootheme/home-client-01.svg" alt="">
+
+        <h3>Perkins & Co.</h3>
+
+
+
+        <p><a href="#"></a></p>
+
+    </li>
+    <li>
+
+        <img src="/wp-content/uploads/yootheme/home-client-02.svg" alt="">
+
+        <h3>Sanowa</h3>
+
+
+
+        <p><a href="#"></a></p>
+
+    </li>
+    <li>
+
+        <img src="/wp-content/uploads/yootheme/home-client-03.svg" alt="">
+
+        <h3>ABC News</h3>
+
+
+
+        <p><a href="#"></a></p>
+
+    </li>
+    <li>
+
+        <img src="/wp-content/uploads/yootheme/home-client-04.svg" alt="">
+
+        <h3>O'well</h3>
+
+
+
+        <p><a href="#"></a></p>
+
+    </li>
+</ul>
+<div>
+
+
+    <h2><b>Varför ska ni välja just oss?</b></h2>
+
+    <p>Fördelar</p>
+
+    <div><div class="elementor-element elementor-element-53747799 elementor-widget elementor-widget-text-editor" data-id="53747799" data-element_type="widget" data-widget_type="text-editor.default">
+            <div class="elementor-widget-container">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+            </div>
+        </div>
+        <div class="elementor-element elementor-element-3931d629 elementor-icon-list--layout-traditional elementor-list-item-link-full_width elementor-widget elementor-widget-icon-list" data-id="3931d629" data-element_type="widget" data-widget_type="icon-list.default">
+            <div class="elementor-widget-container">
+                <ul class="elementor-icon-list-items">
+                    <li class="elementor-icon-list-item"><span class="elementor-icon-list-icon"><i class="far fa-sun" aria-hidden="true"></i></span><span class="elementor-icon-list-text">Suspe ndisse suscipit sagittis leo.</span></li>
+                    <li class="elementor-icon-list-item"><span class="elementor-icon-list-icon"><i class="far fa-sun" aria-hidden="true"></i></span><span class="elementor-icon-list-text">Lorem Ipsum generators on the tend.</span></li>
+                    <li class="elementor-icon-list-item"><span class="elementor-icon-list-icon"><i class="far fa-sun" aria-hidden="true"></i></span><span class="elementor-icon-list-text">Morbi suscipit scelerisque sapien eget</span></li>
+                </ul>
+            </div>
+        </div></div>
+
+    <p><a href="#">Läs mer </a></p>
+
+</div>
+<div>Vad vi gör</div>
+<ul>
+    <li>
+
+        <img src="/wp-content/uploads/yootheme/home-services-01.svg" alt="">
+
+        <h3>Upptäckt och strategi</h3>
+
+
+        <div>Lorem ipsum dolor sit amet, gravida nonna maecenas veritatis vestum.</div>
+
+
+    </li>
+    <li>
+
+        <img src="/wp-content/uploads/yootheme/home-services-02.svg" alt="">
+
+        <h3>Användbarhet</h3>
+
+
+        <div>Lorem ipsum dolor sit amet, gravida nonna maecenas veritatis vestum.</div>
+
+
+    </li>
+    <li>
+
+        <img src="/wp-content/uploads/yootheme/home-services-03.svg" alt="">
+
+        <h3>Utveckling</h3>
+
+
+        <div>Lorem ipsum dolor sit amet, gravida nonna maecenas veritatis vestum.</div>
+
+
+    </li>
+    <li>
+
+        <img src="/wp-content/uploads/yootheme/home-services-04.svg" alt="">
+
+        <h3>Prestanda</h3>
+
+
+        <div>Lorem ipsum dolor sit amet, gravida nonna maecenas veritatis vestum.</div>
+
+
+    </li>
+</ul>
+<img src="/wp-content/uploads/2022/04/pexels-photo-356036-scaled.jpeg" alt="">
+
+<div>Careers</div>
+<h2><strong>Låt oss bygga ett smartare och effektivare elanvändning</strong></h2>
+<div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</div>
+<ul>
+    <li>
+
+
+        <h3>Test </h3>
+
+        <p>128</p>
+
+
+
+    </li>
+    <li>
+
+
+        <h3>Test 2</h3>
+
+        <p>$28M</p>
+
+
+
+    </li>
+    <li>
+
+
+        <h3>Test 3</h3>
+
+        <p>82%</p>
+
+
+
+    </li>
+</ul>
+<div>Vad vi gör</div>
+<h1><b>Energikälla för ett <br />bättre liv</b></h1>
+<ul>
+    <li>
+
+
+        <h3>Solpaneler</h3>
+
+
+        <div><p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit sed deiusmod tempor incididunt ut labore et dolore magna aliqua. Donec scelerisque dolor id nunc dictum.</span></p></div>
+
+
+    </li>
+    <li>
+
+
+        <h3>Miljövänligt</h3>
+
+
+        <div><p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit sed deiusmod tempor incididunt ut labore et dolore magna aliqua. Donec scelerisque dolor id nunc dictum.</span></p></div>
+
+
+    </li>
+    <li>
+
+
+        <h3>Kostnadsförslag</h3>
+
+
+        <div><p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit sed deiusmod tempor incididunt ut labore et dolore magna aliqua. Donec scelerisque dolor id nunc dictum.</span></p></div>
+
+
+    </li>
+</ul>
+<div>
+
+    <button class="uk-button uk-button-primary" type="button" uk-toggle="target: #offcanvas-flip">Button</button>
+
+    <div id="offcanvas-flip" uk-offcanvas="flip: true; overlay: true">
+        <div class="uk-offcanvas-bar">
+
+            <button class="uk-offcanvas-close" type="button" uk-close></button>
+
+            <h3>Title</h3>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+        </div>
+    </div>
+</div>
+<img src="/wp-content/uploads/2022/04/hero.jpg" alt="">
+<img src="/wp-content/uploads/2022/04/pexels-photo-356036-scaled.jpeg" alt="">
+<div>Bättre marknadspriser</div>
+<ul>
+    <li>
+
+
+        <h3>Intrum</h3>
+
+
+
+
+    </li>
+    <li>
+
+
+        <h3>Intrum</h3>
+
+
+
+
+    </li>
+    <li>
+
+
+        <h3>Intrum</h3>
+
+
+
+
+    </li>
+</ul>
+<div><p>Lorum &amp; Ipsum</p></div>
+<h2><strong>Lorem ipsum dolor sit amet</strong></h2>
+<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</div>
+<ul>
+    <li>
+
+        <img src="/wp-content/uploads/yootheme/home-press-01.svg" alt="">
+
+
+        <h3>Home Press 01</h3>
+
+
+
+        <p><a href="#"></a></p>
+
+    </li>
+    <li>
+
+        <img src="/wp-content/uploads/yootheme/home-press-02.svg" alt="">
+
+
+        <h3>Home Press 02</h3>
+
+
+
+        <p><a href="#"></a></p>
+
+    </li>
+    <li>
+
+        <img src="/wp-content/uploads/yootheme/home-press-03.svg" alt="">
+
+
+        <h3>Home Press 03</h3>
+
+
+
+        <p><a href="#"></a></p>
+
+    </li>
+    <li>
+
+        <img src="/wp-content/uploads/yootheme/home-press-04.svg" alt="">
+
+
+        <h3>Home Press 04</h3>
+
+
+
+        <p><a href="#"></a></p>
+
+    </li>
+</ul>
+<ul>
+    <li>
+
+        <img src="/wp-content/uploads/2022/04/1.png" alt="">
+
+        <h3>Get an Overview</h3>
+
+        <p>LORUM & IPSUM</p>
+
+        <div><p><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</span></p></div>
+
+        <p><a href="/index.php?page_id=13"></a></p>
+
+    </li>
+    <li>
+
+        <img src="/wp-content/uploads/2022/04/1.png" alt="">
+
+        <h3>Get an Overview</h3>
+
+        <p>LORUM & IPSUM</p>
+
+        <div><p><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</span></p></div>
+
+        <p><a href="/index.php?page_id=13"></a></p>
+
+    </li>
+    <li>
+
+        <img src="/wp-content/uploads/2022/04/1.png" alt="">
+
+        <h3>Get an Overview</h3>
+
+        <p>LORUM & IPSUM</p>
+
+        <div><p><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</span></p></div>
+
+        <p><a href="/index.php?page_id=13"></a></p>
+
+    </li>
+    <li>
+
+        <img src="/wp-content/uploads/2022/04/1.png" alt="">
+
+        <h3>Get an Overview</h3>
+
+        <p>LORUM & IPSUM</p>
+
+        <div><p><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</span></p></div>
+
+        <p><a href="/index.php?page_id=13"></a></p>
+
+    </li>
+</ul>
+
+<!--more-->
+<?php
+
+get_footer();
